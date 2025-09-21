108078e7cef859150740cc508dd81980fd81992b
Youssef Ahmed Mohamed Desoky <143013456+youssefDesoky@users.noreply.github.com>
2025-09-21 20:29:19 +0300

    Delete Kindergarten Students Distribution directory

--- Kindergarten Students Distribution/js/index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('dotnet-framework').addEventListener('click', function() {
-            window.open(this.querySelector('a').href, '_blank');
-        });
-});
